e3c9763a1c0cca691c6acc14b0526c3670bc118d
Niklas Chaney <58793008+Unsynx@users.noreply.github.com>
2025-09-13 18:25:15 -0400

    Removed backend and uneccesary packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,20 @@
 import { useState } from 'react'
-import axios from 'axios';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.css'
 
 
 function App() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const fetchData = async () => {
-    setLoading(true);
-    setError(null);
-    try {
-      const response = await axios.get('/api');
-      setData(response.data);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={fetchData} disabled={loading}>
-          {loading ? 'Loading...' : 'Fetch Data'}
-        </button>
-        {error && <p className="error">Error: {error.message}</p>}
-        {data && (
-          <div className="data">
-            <h2>Data from Backend:</h2>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
-          </div>
-        )}
+      <div>
+        <div className="card">
+          <h1>Vite + React</h1>
+          <button>
+            Button
+          </button>
+        </div>
       </div>
     </>
   )
